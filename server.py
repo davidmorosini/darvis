b02cf1d4a20d26d61fe76afa82d156b45dda7893
David Morosini <david.morosinineto@gmail.com>
2019-01-10 19:49:08 -0200

    Excluindo projeto no Jupyter e adicionando o servidor em um arquivo .py

---
+++ server.py
@@ -0,0 +1,30 @@
+import json
+
+#Importando o token de configuração com o telegram
+arquivo = open("token.json").read()
+content = json.loads(arquivo)
+
+token = content['token']
+
+
+import telebot
+from datetime import datetime
+
+bot = telebot.TeleBot(token)
+
+#Implementando alguns comandos de ação..
+
+@bot.message_handler(commands=['teste', 'ola'])
+def send_welcome(message):
+    bot.reply_to(message, u"Olá")
+
+@bot.message_handler(commands=['horas'])
+def send_horas(message):
+    h = datetime.now()
+    bot.reply_to(message, u"{}:{}:{}".format(h.hour, h.minute, h.second))
+
+bot.polling()
+
+
+
+
