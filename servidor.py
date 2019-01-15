7a0424883cea3fd94b2884de9ee74c608f830858
David Morosini <david.morosinineto@gmail.com>
2019-01-15 14:53:10 -0200

    retirada da decodificação do servidor

--- servidor.py
+++ servidor.py
@@ -1,8 +1,6 @@
 import json
-from utilidades import number_on_string
-from utilidades import search_string
-from recomenda import show_movies
-from recomenda import search_titles
+import telebot
+from decodificador import decode_msg
 
 
 #Importando o token de configuração com o telegram
@@ -10,48 +8,20 @@ arquivo = open("token.json").read()
 content = json.loads(arquivo)
 token = content['token']
 
-import telebot
 
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(func=lambda txt: (len(txt.text.upper().split('LISTAR')) > 1))
-def show_listar(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Um segundo, deixe me ver os que tenho aqui...")
 
-    text_ = ""
-    
-    #Listar Filmes
-    qtd_filmes = number_on_string(message.text)
-    if(qtd_filmes == None):
-        #colocar a quantidade default
-        qtd_filmes = 10
-        text_ += "Listando uma quantidade default de filmes: \n\n"
-    else:
-        text_ += "Listando os " + str(qtd_filmes) + " como você pediu: \n\n"
+#Recebe qualquer mensagem
+@bot.message_handler(func=lambda m: not((len(m.text.split('/')) > 1)))
+def msg(message):
+    resp = decode_msg(message, bot)
+    bot.send_message(message.chat.id, resp)
 
-    filmes = show_movies(qtd=qtd_filmes)
 
-    for i in range(0, qtd_filmes):
-        text_ += filmes[i] + '\n'
-
-    text_ += '\n\nrecomendar [nome do filme] <para recomendações..>'
-    
-    bot.reply_to(message, u"{}".format(text_))
-
-@bot.message_handler(func=lambda txt: (len(txt.text.upper().split('RECOMENDAR')) > 1))
-def show_listar(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Deixe me pensar...")
-
-    title = message.text.split('recomendar ')[1]
-    text = search_titles(title)
-
-    bot.reply_to(message, u"Aqui estão, espero que goste:\n\n{}".format(text))
-    
-#@bot.message_handler(func=lambda m: True)
-#def echo_all(message):
-#	bot.reply_to(message, message.text)
+@bot.message_handler(commands=['teste'])
+def echo_all(message):
+	bot.reply_to(message, 'comando...')
 
 
 bot.polling()
